751380e79956eaa108013ef0ff9b5bf02c83bd3d
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-11-08 12:42:34 +1300

    Add placeholder / view using Handlebars

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const server = express()
+
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+server.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+server.get('/', (req, res) => {
+  const viewData = {
+    messages: [
+      {
+        content: `Please run the command <code>npm run test</code> to see the tests`,
+      },
+    ],
+  }
+
+  res.render('home', viewData)
+})
+
+module.exports = server
