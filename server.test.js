bdfcc72346669b023fed624d4ff3e203a1fb9254
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-11-08 15:55:40 +1300

    Remove repeated code

--- server.test.js
+++ server.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest')
+const server = 'https://api.tmsandbox.co.nz'
+const route = '/v1/Categories/6328/Details.json?catalogue=false'
 
 describe('TMSandbox API', () => {
   test('API responds with JSON', async () => {
-    const res = await request('https://api.tmsandbox.co.nz').get(
-      '/v1/Categories/6328/Details.json?catalogue=false'
-    )
+    const res = await request(server).get(route)
     expect(res.headers['content-type']).toBe('application/json')
     expect(res.status).toBe(200)
   })
@@ -12,9 +12,7 @@ describe('TMSandbox API', () => {
   test('Name is badges', async () => {
     const expected = 'Badges'
 
-    const res = await request('https://api.tmsandbox.co.nz').get(
-      '/v1/Categories/6328/Details.json?catalogue=false'
-    )
+    const res = await request(server).get(route)
     const actual = res.body.Name
 
     expect(actual).toBe(expected)
@@ -23,9 +21,7 @@ describe('TMSandbox API', () => {
   test('CanRelist is true', async () => {
     const expected = true
 
-    const res = await request('https://api.tmsandbox.co.nz').get(
-      '/v1/Categories/6328/Details.json?catalogue=false'
-    )
+    const res = await request(server).get(route)
     const actual = res.body.CanRelist
 
     expect(actual).toBe(expected)
@@ -34,9 +30,7 @@ describe('TMSandbox API', () => {
   test('The Promotions element with Name = "Feature" has a Description that contains the text "Better position in category"', async () => {
     const expected = 'Better position in category'
 
-    const res = await request('https://api.tmsandbox.co.nz').get(
-      '/v1/Categories/6328/Details.json?catalogue=false'
-    )
+    const res = await request(server).get(route)
 
     const feature = res.body.Promotions.find(
       (promotion) => promotion.Name === 'Feature'
